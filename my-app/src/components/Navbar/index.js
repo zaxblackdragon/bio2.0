8604e6878671fed8f2febe259e582069019a25b3
Zac Seewald <zaxblackdragon.com>
2019-02-04 22:46:02 -0600

    styled the nav and side bars

--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-
+import { css } from 'glamor';
+import "./index.css";
 
 
 export class navbar extends Component {
@@ -15,19 +16,26 @@ export class navbar extends Component {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+      <Nav.Link
+      bsPrefix="navbar"
+      href="#features">Features</Nav.Link>
+      <Nav.Link 
+      bsPrefix="navbar"
+      href="#pricing">Pricing</Nav.Link>
+      <NavDropdown bsPrefix="dropdown-toggle" title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item 
+        bsPrefix="dropdown-item"
+        href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Item 
+        href="#action/3.3">Something</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
+      <Nav.Link bsPrefix="navbar" href="#deets">More deets</Nav.Link>
+      <Nav.Link bsPrefix="navbar" eventKey={2} href="#memes">
         Dank memes
       </Nav.Link>
     </Nav>
@@ -50,3 +58,4 @@ const brandStyle = {
   fontSize: "30px",
   fontWeight: "bold",
 }
+
